fd9f9b55d8afc16ae99976516d6b3dbcdbdaa790
ankys <ankysp@gmail.com>
2025-01-18 16:12:54 +0900

    delete clone

--- src/shuffle.rs
+++ src/shuffle.rs
@@ -28,7 +28,7 @@ pub fn shuffle(initial_state: State) -> Cache {
 		if p >= states.len() {
 			break;
 		}
-		let state = states[p].0.clone();
+		let state = states[p].0;
 		for op in OPERATIONS {
 			let state2 = operate(&state, op);
 			let code2 = encode(&state2);
