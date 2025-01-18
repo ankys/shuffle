1c8bbbc2fd083b6d012d89f52381c3c253666f0d
ankys <ankysp@gmail.com>
2025-01-18 12:10:59 +0900

    add unit benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,14 +1,16 @@
 
 extern crate criterion;
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-extern crate lib;
-use lib::shuffle::shuffle::*;
-use lib::cube2::shuffle_cube2::*;
+extern crate src;
+use src::shuffle::shuffle::*;
+use src::cube2::shuffle_cube2::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
 	let initial_state = get_initial_state();
-	c.bench_function("cube2", |b| b.iter(|| shuffle(initial_state)));
+	c.bench_function("encode", |b| b.iter(|| encode(&black_box(initial_state))));
+	c.bench_function("operate", |b| b.iter(|| operate(&black_box(initial_state), 0)));
+	// c.bench_function("cube2", |b| b.iter(|| shuffle(initial_state)));
 }
 
 criterion_group!(benches, criterion_benchmark);
